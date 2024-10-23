440aa02f432bcbd996bde46627e412e8b1d11d45
Lenia <110248772+Futitisme@users.noreply.github.com>
2024-10-23 18:56:02 +0300

    Update client.cpp

--- lab_server/client.cpp
+++ lab_server/client.cpp
@@ -5,7 +5,8 @@
 #include <thread>
 
 int PORT = 6666;
-char SERVER_IP[] = "192.168.64.1";
+char SERVER_IP[] = "127.0.0.1";
+std::string username;
 
 // Функция для приема сообщений от сервера
 void receiveMessages(int clientSocket) {
@@ -13,8 +14,16 @@ void receiveMessages(int clientSocket) {
     while (true) {
         int valread = read(clientSocket, buffer, 1024);
         if (valread > 0) {
-            std::cout << "\nMessage from another client: " << buffer << std::endl;
-            memset(buffer, 0, sizeof(buffer)); // Очистка буфера после получения сообщения
+            std::string message(buffer);
+            // Выделяем личные сообщения
+            if (message.find('@') == 0) {
+                // Личное сообщение
+                std::cout << "\033[1;32m" << message << "\033[0m\n";  // Зеленый цвет
+            } else {
+                // Обычное сообщение
+                std::cout << message << std::endl;
+            }
+            memset(buffer, 0, sizeof(buffer)); // Очистка буфера
         }
     }
 }
@@ -22,7 +31,7 @@ void receiveMessages(int clientSocket) {
 int main() {
     int clientSocket;
     struct sockaddr_in serverAddress;
-    char message[1024] = {0};
+    std::string message;
 
     // Создание сокета
     if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -47,15 +56,34 @@ int main() {
 
     std::cout << "Connected to server\n";
 
+    // Ввод имени пользователя перед подключением
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, username);
+    send(clientSocket, username.c_str(), username.length(), 0);
+
     // Запуск отдельного потока для приема сообщений от сервера
     std::thread receiveThread(receiveMessages, clientSocket);
     receiveThread.detach(); // Отделяем поток для независимого выполнения
 
     // Цикл отправки сообщений
     while (true) {
-        std::cout << "Enter message: ";
-        std::cin.getline(message, 1024);
-        send(clientSocket, message, strlen(message), 0);
+        std::getline(std::cin, message);
+
+        // Проверка на специальные команды
+        if (message.find("#NewName") == 0) {
+            // Команда для смены имени
+            send(clientSocket, message.c_str(), message.length(), 0);
+        } else if (message.find("#users") == 0) {
+            // Команда для запроса списка пользователей
+            send(clientSocket, message.c_str(), message.length(), 0);
+        } else if (message.find("#ToUser") == 0) {
+            // Команда для отправки личного сообщения
+            send(clientSocket, message.c_str(), message.length(), 0);
+        } else {
+            // Отправка обычного сообщения
+            send(clientSocket, message.c_str(), message.length(), 0);
+        }
+
         std::cout << "Message sent\n";
     }
 
